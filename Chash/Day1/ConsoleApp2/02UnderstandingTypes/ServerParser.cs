908e1b53da1d01e59145ab4e43ac43e1e90b0587
Sanjith <petapallisanjith@gmail.com>
2024-12-06 13:22:14 -0500

    Final Day 1 assignment

--- Chash/Day1/ConsoleApp2/02UnderstandingTypes/ServerParser.cs
+++ Chash/Day1/ConsoleApp2/02UnderstandingTypes/ServerParser.cs
@@ -7,12 +7,48 @@ public class ServerParser
     public static void Run()
     {
         String input = Console.ReadLine();
-        char[] specialCharacters = { '.', ',', ':', ';', '?', '!', '\'', '\"', '(', ')', '[', ']', '\\', '/', '&', '=' };
-        String separators = @"//:";
+        char[] specialCharacters = { '.', ',', ':', ';'};
+        String separators = @"/";
         string[] words = Regex.Split(input.Trim(), separators);
+        
+        List<String> list = new List<String>();
         for (int i = 0; i < words.Length; i++)
         {
-            Console.WriteLine(words[i]);
+            words[i] = words[i].Trim(specialCharacters);
+            if (words[i].Length > 0)
+            {
+                list.Add(words[i]);
+            }
+        }
+
+        String[] arrWords = list.ToArray();
+
+        if (arrWords.Length == 1)
+        {
+            Console.WriteLine("[protocol] = \"\"");
+            Console.WriteLine($"[server] = \"{arrWords[0]}\"");
+            Console.WriteLine("[resource] = \"\"");
+            
+        }
+        else if (arrWords.Length == 3)
+        {
+            Console.WriteLine($"[protocol] = \"{arrWords[0]}\"");
+            Console.WriteLine($"[server] = \"{arrWords[1]}\"");
+            Console.WriteLine($"[resource] = \"{arrWords[2]}\"");
+
+            
+        }
+        else if (arrWords[0].IndexOf(".") >= 0)
+        {
+            Console.WriteLine($"[protocol] = \"\"");
+            Console.WriteLine($"[server] = \"{arrWords[0]}\"");
+            Console.WriteLine($"[resource] = \"{arrWords[1]}\"");
+        }
+        else
+        {
+            Console.WriteLine($"[protocol] = \"{arrWords[0]}\"");
+            Console.WriteLine($"[server] = \"{arrWords[1]}\"");
+            Console.WriteLine($"[resource] = \"\"");
         }
 
     }
